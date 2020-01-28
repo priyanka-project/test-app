d5986bbbe9a7610105d76e4664eee04f399b66f7
John Blesswin A <johnblesswina@Johns-MacBook-Pro.local>
2020-01-28 15:22:07 +0530

    Updates

---
+++ precache-manifest.def8c1594767fee25bfab51b211524e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e62743d1c850820653ec28316b88dd4",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "d901e0713204abbf6be2",
+    "url": "/test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d60f1629cfd06c1c48be",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-app/static/js/2.6199a9ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d901e0713204abbf6be2",
+    "url": "/test-app/static/js/main.a5a5cbac.chunk.js"
+  },
+  {
+    "revision": "a7685168259b13e7085a",
+    "url": "/test-app/static/js/runtime-main.350dc61c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
